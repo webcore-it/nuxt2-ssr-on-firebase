ed7e4d9dfedc6641a48ec7254d0639237385b3bb
webcore-it <info@webcore-it.com>
2018-12-05 00:11:30 +0100

    Use big.js for demonstration of "firebaseFunctionsDependencies".

--- functions/api/redVsBlue.js
+++ functions/api/redVsBlue.js
@@ -1,7 +1,12 @@
 const functions = require('firebase-functions');
+const Big = require('big.js');
 
 // Backend API function to determine the winner of the everlasting fight red vs. blue.
 exports = module.exports = functions.https.onRequest((req, res) => {
+
+  // Example of using a npm package only in Firebase Functions.
+  const thisDoesNothing = Big(123);
+
   const time = new Date().getTime();
   let winner = { time };
 
